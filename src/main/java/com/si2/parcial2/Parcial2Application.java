dd6d28ff2e300ee1ffef8f0d0ef0891222a730ab
jose Alejandro Calzadilla Nogales <78042869ale@gmail.com>
2024-06-07 15:58:45 -0400

    michelnosabe

--- src/main/java/com/si2/parcial2/Parcial2Application.java
+++ src/main/java/com/si2/parcial2/Parcial2Application.java
@@ -2,9 +2,6 @@ package com.si2.parcial2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Parcial2Application {
@@ -13,16 +10,5 @@ public class Parcial2Application {
 		SpringApplication.run(Parcial2Application.class, args);
 	}
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("*")
-						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-						.allowedHeaders("*");
-			}
-		};
-	}
+	
 }
